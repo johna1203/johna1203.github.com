ccc324a9067395d82183908136873254cec982c4
Johnathan David Froeming <johnathan@kodokuman.com>
2013-04-27 15:22:16 -0300

    テスト！

--- assets/themes/kodokux/js/config.js
+++ assets/themes/kodokux/js/config.js
@@ -1,12 +1,6 @@
-/*
-	Verti 2.0 by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 window._skel_config = {
 	preset: 'standard',
-	prefix: 'css/style',
+	prefix: '{{ ASSET_PATH }}/css/style',
 	resetCSS: true
 };
 
